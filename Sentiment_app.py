cc144e2e0bc5532f037902807b06b0a387a25ce7
SarveshKrishnanRajendran <SarveshKrishnanRajendran@users.noreply.github.com>
2024-10-21 01:10:48 -0400

    Edited Final

---
+++ Sentiment_app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+import torch
+
+
+model_path = './saved_model'
+tokenizer = DistilBertTokenizer.from_pretrained(model_path)
+model = DistilBertForSequenceClassification.from_pretrained(model_path)
+
+def classify_tweet(tweet):
+    inputs = tokenizer(tweet, return_tensors="pt", padding=True, truncation=True, max_length=128)
+    with torch.no_grad():
+        outputs = model(**inputs)
+    logits = outputs.logits
+    probs = torch.softmax(logits, dim=-1)
+    return "Positive" if torch.argmax(probs) == 1 else "Negative"
+
+
+st.title("Sentiment Analyzer")
+user_input = st.text_area("Type to analyze:", "Enter here...")
+if st.button("Analyze"):
+    result = classify_tweet(user_input)
+    st.write("Sentiment: ", result)
+
+
+# In[ ]:
+
+
+
+
